61334fca5a5a9fb5dc799023de4c648ffcffafc0
MH Munna <71868847+Monirhasanmunna@users.noreply.github.com>
2023-10-04 14:28:05 +0600

    side bar active when chage route

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+
 </script>
 <template>
     <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
         <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul class="space-y-2 font-medium">
-                <li>
+                <li :class="{ active : $route.name == 'dashboard'}">
                     <router-link to="/dashboard" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                             <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
@@ -14,7 +15,7 @@ import { RouterLink } from 'vue-router';
                         <span class="ml-3">Dashboard</span>
                     </router-link>
                 </li>
-                <li>
+                <li :class="{ active : $route.name == 'products'}">
                     <router-link to="/products" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                             <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
@@ -27,3 +28,10 @@ import { RouterLink } from 'vue-router';
     </aside>
 
 </template>
+
+<style scoped>
+    .active{
+        background-color: #c5c5c5a8;
+        border-radius: 7px;
+    }
+</style>
